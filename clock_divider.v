72fe9cad37d7728ef0e4b5b6f2e82de2247e81ec
Youngchan Cho <91108091+youngchanlouiecho@users.noreply.github.com>
2022-04-25 16:16:26 -0400

    Add files via upload

--- clock_divider.v
+++ clock_divider.v
@@ -1,19 +1,19 @@
 `timescale 1ns / 1ps
 
-module clock_divider(clock_in, clock_out, sec_in);
-    input clock_in, sec_in;
-    output reg clock_out;
+module clock_divider(clock_high_hz, clock_1hz, sec_in);
+    input clock_high_hz, sec_in;
+    output reg clock_1hz;
 
     reg [27:0] counter = 28'd0;
     parameter DIVISOR = 28'd50000000; // the DIVISOR should be 50,000,000
     
-    always @ (posedge clock_in)
+    always @ (posedge clock_high_hz)
     begin
     counter <= counter + 28'd1;
     
     if (counter >= (DIVISOR - 1))
         counter <= 28'd0;
-        clock_out <= (counter < DIVISOR / 2) ? 1'b1 : 1'b0;
+        clock_1hz <= (counter < DIVISOR / 2) ? 1'b1 : 1'b0;
     end
 endmodule
 
--- countdown.v
+++ countdown.v
@@ -1,36 +1,50 @@
-module CntDwn(input clock_out, input[4:0]seconds, output[4:0]countdown);
-    input pause, start, restart, reset;
-    reg[4:0]dif=0;//Difference
+module CntDwn(clock_1hz, seconds, countdown, pause, start, restart, reset);
+    input clock_1hz;
 
-    case (pause_start)
-        (pause == 1): state = 1;
-        (start == 1): state = 2;
-        (restart == 1): state = 3;
-        (reset == 1): state = 4;
-        default: state = 1;
-    endcase
+    input [5:0] seconds
+    output reg [5:0] countdown // 6 bit input/output
+    countdown <= seconds;
 
-    always@(posedge clock_out)
-    if (state == 1)
-        assign countdown = countdown;
+    input pause, start, restart, reset; // buttons
 
-    else if (state == 2)
+    reg [5:0] dif = 0; // difference
+    reg state;
+    state <= 1; // default state is pause
+
+    always@(posedge clock_1hz)
     begin
-        if(dif==seconds)
-            dif<=0;
-        else
-            dif<=dif+1;
-        assign countdown=cd-dif;
+        if (pause == 1) // when 'pause' button pressed
+            state <= 1;
+        else if (start == 1) // when 'start' button pressed
+            state <= 2;
+        else if (restart == 1) // when 'restart' button pressed
+            state <= 3;
+        else if (reset == 1) // when 'reset' button pressed
+            state <= 4;
+
+
+
+        if (seconds == 0)
+            assign countdown = 0; // when timer 0, remain 0
+
+        else if (state == 1)
+            assign countdown = countdown; // timer paused
+
+        else if (state == 2)
+        begin
+            if (dif == seconds)
+                dif <= 0;
+            else
+                dif <= dif + 1;
+            assign countdown = seconds - dif; // timer -1 every seconds
+        end
+
+        else if (state == 3)
+            assign countdown = seconds; // timer to initial input
+
+        else if (state == 4)
+            assign countdown = 0; // clear output
     end
-
-    else if (state == 3)
-        assign countdown = seconds;
-
-    else if (state == 4)
-        assign countdown = 0;
 endmodule
 
 
-
-// the 'reset' button should start the system from the initial state
-// I guess the inputs should be initialized too
